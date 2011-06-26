59dce5fb0fa1cdaeb38698c5becdc983494ce404
linjian <lingcloud@users.noreply.github.com>
2011-06-26 12:15:51 +0000

    fix the browser name bug.

--- common/trunk/src/test/portal/org/lingcloud/molva/test/util/TestConstants.java
+++ common/trunk/src/test/portal/org/lingcloud/molva/test/util/TestConstants.java
@@ -64,7 +64,7 @@ public final class TestConstants {
 	  *custom
 	*/
 		public static final String FIREFOX = "*firefox";
-		public static final String CHROME = "*chrome";
+		public static final String CHROME = "*googlechrome";
 		public static final String IE = "*iexplore";
 	}
 }
