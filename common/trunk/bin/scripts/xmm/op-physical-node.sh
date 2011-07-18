7c79e4d215bb672c9bfe6dfa36faf35bff43c01e
linjian <lingcloud@users.noreply.github.com>
2011-07-18 09:19:50 +0000

    add xmm scripts.

---
+++ common/trunk/bin/scripts/xmm/op-physical-node.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (C) 2008-2011,
+# LingCloud Team,
+# Institute of Computing Technology,
+# Chinese Academy of Sciences.
+# P.O.Box 2704, 100190, Beijing, China.
+#
+# http://lingcloud.org
+#
+
+failed="false"
+sucess="true"
+
+function stop_pnnode
+{
+	# $1 is the ip of the physical machine
+	ssh $1 halt -p
+}
+function start_pnnode
+{
+	# $1 is the mac of the pnysical machine
+	etherwake $1
+}
+
+if [ $2 = "stop" ] ; then
+	stop_pnnode $1;
+else
+	start_pnnode $1;
+fi
+echo "$sucess"
+exit 0
