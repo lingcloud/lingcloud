f1892af36152fe33e0932c1c83631b0346269da3
linjian <lingcloud@users.noreply.github.com>
2011-07-19 13:32:57 +0000

    fix bugs in physical node scripts.

--- common/trunk/bin/scripts/xmm/op-physical-node.sh
+++ common/trunk/bin/scripts/xmm/op-physical-node.sh
@@ -16,49 +16,48 @@ function stop_pnnode
 {
 	# $1 is the ip of the physical machine
 	ssh $1 halt -p
-	status=$?
-	if [ $status != "0" ]
-		then { echo "$failed";  }
-	else {
-		echo "$sucess";
-	}
+	if [ "$?" != "0" ]
+	then
+		echo "$failed"
+	else
+		echo "$sucess"
+	fi
 }
+
 function start_pnnode
 {
 	# $1 is the mac of the physical machine
 	ether-wake $1
-	status=$?
-	if [ $status != "0" ]
-		then { echo "$failed";  }
-	else {
-		echo "$sucess";
-	}
+	if [ "$?" != "0" ]
+	then
+		echo "$failed"
+	else
+		echo "$sucess"
+	fi
 }
+
 function ping_pnnode
 {
 	# $1 is the ip of the physical machine
-	ping -c 5 $1 >/dev/null  2>/dev/null
-	status=$?
-	if [ $status != "0" ]
-		then { echo "$failed";  }
-	else {
-		echo "$sucess";
-	}
+	ping -c 5 $1 >/dev/null 2>/dev/null
+	if [ "$?" != "0" ]
+	then
+		echo "$failed"
+	else
+		echo "$sucess"
+	fi
 }
 
-
-if [ $2 = "stop" ] 
- then
-	stop_pnnode $1;
+if [ "$2" = "stop" ]
+then
+	stop_pnnode $1
 else
-	{
-		if [ $2 = "start" ] 
-			then {
-				start_pnnode $1;
-			}
-		else {
-			ping_pnnode $1;
-		} 
-	}
+	if [ "$2" = "start" ]
+	then
+		start_pnnode $1
+	else
+		ping_pnnode $1
+	fi
 fi
+
 exit 0
