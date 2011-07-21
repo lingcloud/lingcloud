57ed9a2244cd701673a290b9df67cea56e438c0f
liliang <lingcloud@users.noreply.github.com>
2011-07-21 12:20:21 +0000

    Improve the efficiency of the collector shell - static-metaInfo-collector.sh.

--- common/trunk/bin/scripts/xmm/static-metaInfo-collector.sh
+++ common/trunk/bin/scripts/xmm/static-metaInfo-collector.sh
@@ -25,51 +25,8 @@ get_interfaces() {
 }
 
 get_interface_ip() {
-    NUM=`$IFCMD | wc -l`
-    names=`get_interfaces_name`
-    #echo ${name_array[1]}
-    tag=1
-    index=1
-    result=""
-    for ((i=1;i<=$NUM;i++))
-    do
-      line=`$IFCMD | sed -n "$i p"`
-      prefix=`echo $line | cut -d" " -f1`
-      cur_name=`echo $names | cut -d" " -f$index`
-      #echo cur_name=$cur_name
-      #echo $prefix $cur_name
-      if [ "$prefix" == "$cur_name" ]; then
-	#Indicates we need to get the next line of IP.
-	#echo $cur_name
-	nicmac=`get_interfaces $cur_name`
-	if [ x"$nicmac" == "x" ]; then
-		continue;
-	fi
-	#echo "nicmac=$nicmac*"
-	if [ x"$result" == "x" ]; then
-		result=$nicmac
-	else 
-		result=$result" "$nicmac
-	fi
-	#result=$result" "$nicmac
-	#echo $result
-	tag=0
-	continue
-     fi
-     if [ $tag -eq 0 ]; then
-	tag=1
-	#FIXME Some interface doesn't contain ip address.
-	result_tmp=`echo $line | awk -F"inet addr:" '{print $2}' | tr -s ' ' | cut -d" " -f1`
-	if [ "$result_tmp" == "" ]; then
-	  result=$result"-"$IP
-	else
-	  result=$result"-"$result_tmp
-	fi
-	index=`expr $index + 1`
-     fi
-     done
-     #echo $result | tr -s ' '
-     echo $result
+    $IFCMD | awk 'BEGIN{}/HWaddr|inet/{ if ($2 == "Link"){ if (flag == 1) if (flag == 1) printf "-0.0.0.0 "; printf $1"-"$5;flag=1;} if ($1 == "inet") { printf "-"substr($2,6)" "; flag=0;} }END{ if (flag == 1) printf "-0.0.0.0"}'
+    return 0
 }
 
 ARCH=`uname -m`
